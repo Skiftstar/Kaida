f0939d490b9ffff3bb151420bcfa9f853ca6b376
Skiftstar <yukamitft@gmail.com>
2025-05-20 17:53:38 +0200

    make embeddings editable

--- react/app/components/Popups/PermanentEmbeddingsPopup.tsx
+++ react/app/components/Popups/PermanentEmbeddingsPopup.tsx
@@ -2,6 +2,10 @@ import type { Embedding } from '~/types'
 import { Popup } from '../Popup'
 import { useState, type FormEvent } from 'react'
 import { deleteUserEmbedding } from '~/util/Api'
+import EditIcon from '@mui/icons-material/Edit'
+import ClearIcon from '@mui/icons-material/Clear'
+import DoneIcon from '@mui/icons-material/Done'
+
 export function PermanentEmbeddingsPopup({
   open,
   onClose,
@@ -14,6 +18,9 @@ export function PermanentEmbeddingsPopup({
   refetch: () => void
 }) {
   const [actionPending, setActionPending] = useState(false)
+  const [currentEditId, setCurrentEditId] = useState<number | undefined>(
+    undefined
+  )
 
   const handleEmbeddingDelete = async (embeddingId: number) => {
     setActionPending(true)
@@ -39,16 +46,48 @@ export function PermanentEmbeddingsPopup({
           {embeddings.map((embedding) => {
             return (
               <div className="flex gap-2 items-center pb-1 px-1">
-                <span>{embedding.text}</span>
-                <button
-                  disabled={actionPending}
-                  onClick={() => {
-                    handleEmbeddingDelete(embedding.id)
-                  }}
-                  className="ml-auto mr-5 cursor-pointer p-[2px] !text-red-700"
-                >
-                  X
-                </button>
+                {currentEditId === embedding.id ? (
+                  <input
+                    className="textInput p-2 rounded w-full h-[20px] !text-lg"
+                    value={embedding.text}
+                  />
+                ) : (
+                  <span>{embedding.text}</span>
+                )}
+                {currentEditId === embedding.id ? (
+                  <div className="ml-auto flex">
+                    <button className="text-green-700">
+                      <DoneIcon />
+                    </button>
+                    <button
+                      className="mr-5 !text-red-700"
+                      onClick={() => {
+                        setCurrentEditId(undefined)
+                      }}
+                    >
+                      <ClearIcon />
+                    </button>
+                  </div>
+                ) : (
+                  <div className="ml-auto flex">
+                    <button
+                      onClick={() => {
+                        setCurrentEditId(embedding.id)
+                      }}
+                    >
+                      <EditIcon />
+                    </button>
+                    <button
+                      disabled={actionPending}
+                      onClick={() => {
+                        handleEmbeddingDelete(embedding.id)
+                      }}
+                      className="mr-5 !text-red-700"
+                    >
+                      <ClearIcon />
+                    </button>
+                  </div>
+                )}
               </div>
             )
           })}
